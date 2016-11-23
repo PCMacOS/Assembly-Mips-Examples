42ef5969a59d1b36f276b6ded128a841059f6d49
Panagiotis-Stephanos Makos <PCMacOS@users.noreply.github.com>
2016-11-23 12:34:57 +0200

    Update and rename Askisi1a_icsd15111.s to Example-1/Askisi1a_icsd15111.s
    Exercise 1 A
    Questions 
    1. Write a program that displays your name and the registration number you, in two consecutive lines in console QtSpim.

---
+++ Example-1/Askisi1a_icsd15111.s
@@ -0,0 +1,31 @@
+# Example 1 A' Panagiotis-Stephanos Makos icsd15111
+
+.data                        # directive of assemply, from here the variables declared
+
+str: .asciiz "Panagiotis-Stephanos Makos\n"    #variable "str" name, string type ascii characters in the " ", the price we pay, the ".asciiz" tells us the type of the variable
+str1: .asciiz "321/2015111, icsd15223"  #similarly to the variable str1
+
+
+
+
+.text   # the program is placed below the directive .text
+
+main:    #from here starts the main program
+
+la $a0,str # to print a string must first save the address of the registrar $a0, namely here the command "la" stores the address of the variable "στρ" in register $a0
+               
+
+li $v0,4 # is price system call for printing a string, then 4 is the system call code, stores the value of the variable str, in register $v0, this essentially makes the command li
+           
+            
+syscall  # prints the string, the value of the variable "str", this command executes a system call
+     
+
+la $a0,str1 # again if you print must store the address of str1 in register "$ao", like the command "la"
+              
+li $v0,4  #again becomes a system call to print string, like 4, the command "li" loads the value of the variable str1, in register $v0
+syscall   # prints the string, price of str1,command of MIPS, performs system call
+
+
+li $v0,10  #Here's call system with code 10 corresponds to the output of the system 
+syscall   # ends execution with the command syscall
